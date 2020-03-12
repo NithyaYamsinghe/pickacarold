9538da84fd9c21fc4fbd80ed64205872099aa050
hirupiyumika <55011257+hirupiyumika@users.noreply.github.com>
2020-03-12 06:40:32 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/pickacar/MainActivity.java
+++ app/src/main/java/com/example/pickacar/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.WindowManager;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
