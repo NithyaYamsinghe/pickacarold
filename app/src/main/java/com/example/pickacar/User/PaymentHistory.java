616a2f23f8db1629a43de48bb4db8f099042c8af
hirupiyumika <hirupiyumika@gmail.com>
2020-03-19 09:38:57 +0530

    add my journy details

--- app/src/main/java/com/example/pickacar/User/PaymentHistory.java
+++ app/src/main/java/com/example/pickacar/User/PaymentHistory.java
@@ -2,15 +2,25 @@ package com.example.pickacar.User;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.pickacar.R;
 
 public class PaymentHistory extends AppCompatActivity {
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_history);
+    }
+
+    public void onClickDetails(View view) {
+        Intent i = new Intent(this, Journey.class);
+        startActivity(i);
     }
 }
